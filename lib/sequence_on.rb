c356ee1e198d8a595736c864e49e47f89de8bde8
Jérémy SEBAN <jeremy@seban.eu>
2017-05-18 11:50:39 +0200

    Init repo

---
+++ lib/sequence_on.rb
@@ -0,0 +1,35 @@
+module SequencedOn
+  extend ActiveSupport::Concern
+
+  class_methods do
+
+    def sequence_on(l)
+      @@seq_on = l
+    end
+  end
+
+  included do
+    before_create :generate_sequence_id, prepend: true
+
+    private
+
+    def postgresql?
+      defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) &&
+        self.class.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+    end
+
+    def generate_sequence_id
+      if postgresql?
+        self.class.connection.execute("LOCK TABLE #{self.class.table_name} IN EXCLUSIVE MODE")
+      end
+      last_record = self.class.class_exec(self, &@@seq_on).
+        order("sequential_id DESC").
+        first
+      self.sequential_id = if last_record
+                             last_record.sequential_id + 1
+                           else
+                             1
+                           end
+    end
+  end
+end
---
+++ sequence_on.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'sequence_on'
+  s.version = '0.0.1'
+  s.date = '2017-05-18'
+  s.summary = 'Flexible acts_as_sequenced replacement'
+  s.description = 'Flexible acts_as_sequenced replacement'
+  s.authors = ['Jérémy SEBAN']
+  s.email = 'dev@qonto.eu'
+  s.files = Dir['lib/**/*']
+  s.license = 'MIT'
+  s.homepage = 'https://github.com/qonto/sequence_on'
+  s.required_ruby_version = '~> 2.3'
+end
